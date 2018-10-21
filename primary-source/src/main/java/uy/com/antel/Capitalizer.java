064de88452ef9990049d1f6cd4d4799e3af0cbba
Goday Soria, José Leonel <jgodaysoria@antel.com.uy>
2018-10-21 11:16:23 -0300

    Comunicación con Terminal funcionando. Hay que ejecutar la funcion para crear un nuevo ticket

--- primary-source/src/main/java/uy/com/antel/Capitalizer.java
+++ primary-source/src/main/java/uy/com/antel/Capitalizer.java
@@ -18,61 +18,74 @@ public class Capitalizer extends Thread {
         System.out.println("Se genero una nuevo coexión núm: "+clientNumber);
     }
 
+    /**
+     * Empieza a funcionar el hilo de comunicación.
+     * Primero recive los datos.
+     * Luego crea el ticket
+     * Por ultimo responde a la petición.
+     */
     public void run(){
-        System.out.println("Empieza a correr un nuevo Hilo.");
-        // takes input from the client socket
-        try {
-            in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
-
-        BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String line = "";
-
-        // reads message from client until "Over" is sent
-        System.out.println("entro");
-        boolean flagRead = true;
-        while (flagRead)
-        {
-            if(line != null){
-                System.out.println("do something");
-                try
-                {
-                    line = input.readLine();
-                    flagRead = false;
-                    System.out.println("La consola Lee: "+line);
-
-                }
-                catch(IOException i)
-                {
-                    System.out.println("cayo la lectura.");
-                    System.out.println(i);
-                }
-            }
-        }
-            System.out.println("Aqui genero el ticket"+line);
-        //respondo OK + idTiket o False;
-        this.sendMessage("muy bien}");
-        System.out.println("Closing connection");
-        // close connection
-        clientSocket.close();
-        input.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String line = this.reciveMessage();
+        String msg = this.crearTicket(line);
+        this.sendMessage(msg);
     }
 
-
+    /**
+     * Función que recive un mensaje, y lo envía por el socket que está generado.
+     * @param msg
+     */
     private void sendMessage(String msg){
         try {
-            System.out.println("Intento de envío");
             PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true);
-            pw.println("hello world");
-            pw.close();
-//            output = new DataOutputStream(clientSocket.getOutputStream());
-  //          output.writeBytes(msg);
-            //output.close();
+            pw.println(msg);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Función que escucha la comunicación del socket hasta recibir un texto
+     * @return devuelve el texto leido en el socket
+     */
+    private String reciveMessage(){
+        String line = "";
+        try {
+            in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            boolean flagRead = true;
+            while (flagRead)
+            {
+                if(line != null){
+                    try
+                    {
+                        line = input.readLine();
+                        flagRead = false;
+                    }
+                    catch(IOException i)
+                    {
+                        System.out.println("cayo la lectura.");
+                        System.out.println(i);
+                    }
+                }
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return line;
+    }
+
+
+    /**
+     *en esta función se crea el ticket
+     * Devuelve el texto que se debe responder a la terminal
+     * false: La venta del ticket no pudo realizarse.
+     * true: El ticket se generó correctamente: id = XXXXX
+     * siendo XXXX el id del ticket generado;
+     */
+    private String crearTicket(String jsonData){
+        String response = "La venta del ticket no pudo realizarse.";
+        return response;
+    }
+
 
 }
